748b12aeef19e1587ce7e76b2d61ea7491a8783d
victorhenriq <47544612+victorhenriq@users.noreply.github.com>
2019-11-21 19:04:54 -0300

    Add files via upload

---
+++ conexaocombdprof/www/js/firebaseAppInit.js
@@ -0,0 +1,9 @@
+firebaseAppInit.js //
+
+//https://firebase.google.com/docs/web/setup?hl=pt-br
+// Initialize Firebase
+
+function onLoadFunction() {
+    firebase.initializeApp(fbConfig);
+    showCurrentUser(); //update lbUserLoggedIn label
+}
